8349f40c91eca9ed8461e109fea5e29cc27c8c60
maciekkobierecki <mkobierecki@pretius.pl>
2020-01-23 09:02:14 +0100

    App init

---
+++ src/main/java/com/mk/twinodemo/TwinodemoApplication.java
@@ -0,0 +1,13 @@
+package com.mk.twinodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinodemoApplication.class, args);
+	}
+
+}
